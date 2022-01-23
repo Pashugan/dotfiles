3b5d1ba57266cbd2a55f4d9226b1a96c7a28ff30
Pavel Knoblokh <Pashugan@users.noreply.github.com>
2022-01-23 13:35:28 +1000

    fixup

--- firefox/update_profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-readonly SCRIPT_DIR
-
-echo "$SCRIPT_DIR/user.js/update.sh -ldo $SCRIPT_DIR/../private/firefox/user-overrides.js"
